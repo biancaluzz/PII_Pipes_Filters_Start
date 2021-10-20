4559e5435d3c0f883ebbb645a15373da6e546dd9
Bianca Luzzatto <bianca.luzzatto@correo.ucu.edu.uy>
2021-10-19 23:36:49 -0300

    Pipes y Filter

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,25 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            PipeNull pipe3 = new PipeNull();
+
+            IFilter filterTwitter = new FilterConditional();
+            PipeSerial pipeTwitter = new PipeSerial(filterTwitter, pipe3);
+
+            IFilter filter2 = new FilterNegative(); 
+            PipeSerial pipe2 = new PipeSerial(filter2, pipeTwitter);
+
+            IFilter filterPersist = new FilterPersist();
+            PipeSerial pipePersist = new PipeSerial(filterPersist, pipe2);
+
+            IFilter filter1 = new FilterGreyscale();
+            PipeSerial pipe1 = new PipeSerial(filter1, pipePersist);
+
+            IPicture sendPicture = pipe1.Send(picture);
         }
     }
 }
+
